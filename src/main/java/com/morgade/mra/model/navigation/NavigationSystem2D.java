e73ca3be7d76a08b076f98495b8db287ebf8e157
Marcelo Burgos Morgade Cortizo <morgade@gmail.com>
2019-09-24 08:23:08 -0300

    Added NavigationSystem2D interface

---
+++ src/main/java/com/morgade/mra/model/navigation/NavigationSystem2D.java
@@ -0,0 +1,37 @@
+package com.morgade.mra.model.navigation;
+
+import com.morgade.mra.model.Direction2D;
+import com.morgade.mra.model.Position2D;
+
+/**
+ * Define a contract interface for a simple 2D navigation system
+ * 
+ * @author Marcelo Morgade
+ */
+public interface NavigationSystem2D {
+    /**
+     * @return The current position tracked by the navigation system
+     */
+    Position2D getCurrentPosition();
+    
+    /**
+     * @return The current direction tracked by the navigation system
+     */
+    Direction2D getCurrentDirection();
+    
+    /**
+     * Request the navigation system to move based on the current direction
+     * @throws NavigationException when the movement requested is not possible
+     */
+    void move();
+    
+    /**
+     * Request the navigation system to turn left based on the current direction
+     */
+    void turnLeft();
+    
+    /**
+     * Request the navigation system to turn right based on the current direction
+     */
+    void turnRight();
+}
